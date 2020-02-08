f710858f4cac217f591889a2d1bde1985c3ab767
Daniel Zammit <dzamm20@gmail.com>
2020-02-08 20:26:47 +0000

    Updated project files
    Recent files uploaded

---
+++ Assets/Sword.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sword : MonoBehaviour
+{
+    // Public Variables
+
+    // Private Variables
+    private Quaternion startAngle;
+    private Animator swordAnimator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startAngle = transform.rotation;
+        swordAnimator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("Started!"); 
+            SwingSword(0.5f);
+        }
+    }
+
+    private void SwingSword(float delay)
+    {
+        StartCoroutine(Swing(delay));
+    }
+
+    private IEnumerator Swing(float delay)
+    {
+        // play animation from animator
+        swordAnimator.SetBool("Swing", true); 
+        yield return new WaitForSeconds(delay);
+        swordAnimator.SetBool("Swing", false);
+        StopCoroutine(Swing(delay));
+    }
+}
