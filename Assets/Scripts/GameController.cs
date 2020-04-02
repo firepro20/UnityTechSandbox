2481784b28ddcd02c62e7fe3f6a4124857969683
Daniel Zammit <dzamm20@gmail.com>
2020-04-02 06:10:53 +0100

    Update

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    private static GameController instance;
+    public static GameController Instance { get { return instance; } }
+
+    // Class References //
+
+
+    // Public Variables //
+    // Current Speed UI
+    public TMPro.TextMeshProUGUI currentPlayerSpeed;
+
+    // Private Variables //
+    // Game Logic
+    private int gameTime;
+
+    void Awake()
+    {
+        /* Singleton */
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            DestroyImmediate(gameObject);
+        }
+        /* End of Singleton */
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Initialize all variables
+        gameTime = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Convert Time from float to int
+        gameTime = (int)Time.time;
+        
+        // Check Input every 5 seconds since start of the game
+        if (gameTime % 5 == 0)
+        {
+            CheckInputDevices();
+        }
+
+        // Updates Speed Text on UI
+        currentPlayerSpeed.text = String.Format("Speed {0:F0}", PlayerController.Instance.GetPlayerSpeed());
+
+        Quit();
+    }
+
+    // Checks for connected controllers
+    public bool CheckInputDevices()
+    {
+        bool deviceConnected = false;
+        //Get Joystick Names
+        string[] temp = Input.GetJoystickNames();
+        
+        //Check whether array contains anything
+        if (temp.Length > 0)
+        {
+            //Iterate over every element
+            for (int i = 0; i < temp.Length; ++i)
+            {
+                //Check if the string is empty or not
+                if (!string.IsNullOrEmpty(temp[i]))
+                {
+                    //Not empty, controller temp[i] is connected
+                    Debug.Log("Controller " + i + " is connected using: " + temp[i]);
+                    deviceConnected = true;
+                }
+                else
+                {
+                    //If it is empty, controller i is disconnected
+                    //where i indicates the controller number
+                    Debug.Log("Controller: " + i + " is disconnected.");
+                    deviceConnected = false;
+                }
+            }
+        }
+        return deviceConnected;
+    }
+
+    private void Quit()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+
+}
---
+++ Assets/Scripts/SwordController.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwordController : MonoBehaviour
+{
+    private static SwordController instance;
+    public static SwordController Instance { get { return instance; } }
+
+    // Public Variables
+    public float throwForce = 800f;
+    public GameObject sword;
+    public Transform shootPoint;
+    
+
+    // Internal Variables
+    private Vector3 originalPosition;
+    private Quaternion originalRotation;
+    private Vector3 originalScale;
+    private Transform swordParent;
+    private Transform rootParent;
+    private SphereCollider swordCollider;
+    
+    private bool isHolding = true;
+    private bool swordPickupDelay = false;
+    private float pickupDelay = 0.7f;
+
+
+    void Awake()
+    {
+        /* Singleton */
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            DestroyImmediate(gameObject);
+        }
+        /* End of Singleton */
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        originalPosition = transform.localPosition; 
+        originalRotation = transform.localRotation;
+        originalScale = transform.localScale;
+        Debug.Log("Information - " + originalPosition + "\t" + originalRotation + "\t" + originalScale);
+        swordCollider = GetComponentInChildren<SphereCollider>();
+        if (!swordCollider)
+        {
+            Debug.Log("Sword Collider not retrieved!");
+        }
+        swordParent = transform.parent;
+        rootParent = transform.root;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // check if isHolding
+        if (isHolding)
+        {
+            sword.transform.localPosition = originalPosition;
+            sword.transform.localRotation = originalRotation;
+            sword.transform.localScale = originalScale;
+            sword.GetComponent<Rigidbody>().velocity = Vector3.zero;
+            sword.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+            sword.GetComponent<Rigidbody>().useGravity = false;
+            sword.transform.SetParent(swordParent); // right wrist
+            Time.timeScale = 1f;
+        }
+        else
+        {
+            sword.transform.SetParent(null);
+            Time.timeScale = 0.4f;
+        }
+    }
+
+    private void LateUpdate()
+    {
+        if (transform.position.y < -20f)
+        {
+            transform.position = Vector3.one;
+        }
+    }
+
+    // Hits player, set sword to original holding position
+    private void OnCollisionEnter(Collision collision)
+    {
+        // Damage enemies
+
+        /*
+        // go through all contact points and find out which child collider was involved in collisio
+        Collider myCollider = collision.contacts[0].thisCollider;
+        if (myCollider.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Sword Picked up!");
+            
+        }
+        */
+    }
+
+    // called once when object enters trigger
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player") && !isHolding && swordPickupDelay)
+        {
+            Debug.Log("Sword Picked Up!");
+            isHolding = true;
+            swordPickupDelay = false;
+            PlayerController.Instance.playerAnimator.SetInteger("attackID", -1); // change to pickup, change to idle?
+        }
+    }
+
+    public void ThrowSword()
+    {
+        if (isHolding) 
+        {
+            StartCoroutine(SwordPickupDelay(pickupDelay));
+            isHolding = false;
+            Transform tempParent = swordParent;
+            swordParent = rootParent; // shoot from Player transform, which is the root parent
+            sword.transform.position = shootPoint.position;
+            sword.transform.rotation = new Quaternion(Camera.main.transform.rotation.x, swordParent.transform.rotation.y, swordParent.transform.rotation.z, swordParent.transform.rotation.w);
+            //Vector3 localForward = transform.parent.InverseTransformDirection(transform.forward);
+            sword.GetComponent<Rigidbody>().useGravity = true;
+            sword.GetComponent<Rigidbody>().detectCollisions = true;
+            float forceMultiplier = 2f; // 0f
+            
+            sword.GetComponent<Rigidbody>().AddForce(Camera.main.transform.forward * throwForce * forceMultiplier); // swordParent.transform.forward // fires from pivot3
+                                                                                                                  
+            // Call animator to play sword throw clip
+            PlayerController.Instance.playerAnimator.Play("UI_Main_Sword_Throw", 1, 0.5f);
+
+            // reset parent
+            swordParent = tempParent;
+        }
+    }
+
+    public void SwingSword()
+    {
+
+    }
+
+    private IEnumerator SwordPickupDelay(float pickupDelay)
+    {
+        yield return new WaitForSeconds(pickupDelay);
+        swordPickupDelay = true;
+        StopCoroutine(SwordPickupDelay(pickupDelay));
+    }
+
+    public bool GetHoldingStatus()
+    {
+        return isHolding;
+    }
+}
