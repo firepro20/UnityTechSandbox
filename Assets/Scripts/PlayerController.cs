879b2d100d328f914a0ed645d77cdd34ab8508f5
Daniel Zammit <dzamm20@gmail.com>
2020-04-30 03:03:49 +0100

    Update v7

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,458 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    private static PlayerController instance;
+    public static PlayerController Instance { get { return instance; } }
+
+    // Class References //
+    public CharacterController characterController;
+
+    // Public Variables //
+    // Player Movement
+    public float playerSpeed = 5f;
+    public float playerHealth = 100f;
+    public float acceleration = 5f;
+    public float maxPlayerSpeed = 20f;
+    public float gravity = -9.81f;
+    public float jumpHeight = 3f;
+    public float dashDistance = 15f;
+    public float dashCooldown = 4f;
+    public float throwCooldown = 6f;
+    public Transform groundCheck; // empty GroundCheck object
+    public float groundDistance = 0.4f; // radius of physics check sphere
+    public LayerMask groundMask; // only check for ground
+
+    // Sword Control
+    public GameObject playerSword;
+
+    // Animator
+    public Animator playerAnimator;
+
+    // Speedometer
+    public Image speedometer;
+
+    // Clip Duration
+    private float dashClip, deathClip, firstSlashClip, poseClip, secondSlashClip,
+                  shieldClip, spinClip, swordPickupClip, swordThrowClip, swordlessIdleClip, swordlessWalkClip;
+    // Clip List
+    private AnimationClip[] clips;
+    // Layer List
+    //private AnimatorControllerLayer[] layers;
+
+    // Private Variables //
+    // Input Movement
+    private float xAxis;
+    private float zAxis;
+    private float currentPlayerSpeed;
+    private float speedRatio;
+    private Vector3 velocity;
+    private float dashTimer;
+    private float throwTimer;
+
+    // Player Health
+    private float currentPlayerHealth;
+
+    // Sword Control
+    private bool isSwinging;
+    private bool isFirstSlash;
+
+    // Checkpoint 
+    private Vector3 lastPosition; // last position of player
+
+    // Movement Flags
+    private bool toggleDash;
+    private bool isGrounded;
+
+    // Dash Light
+    Light dashLight;
+    Color colorRed = Color.red;
+    Color colorGreen = Color.green;
+
+    void Awake()
+    {
+        /* Singleton */
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            DestroyImmediate(gameObject);
+        }
+        /* End of Singleton */
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Initialize all variables
+        currentPlayerSpeed = playerSpeed;
+        speedRatio = 0f;
+        lastPosition = new Vector3(0f, 0f, 0f);
+        dashTimer = dashCooldown;
+        isSwinging = false;
+        isFirstSlash = true;
+        if (!playerSword)
+        {
+            Debug.Log("Player Sword is not set in inspector!");
+        }
+        currentPlayerHealth = playerHealth;
+        UpdateAnimationClipTimes();
+
+        //UpdateLayerWeight();
+
+        // Dash light
+        dashLight = GetComponentInChildren<Light>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GameController.Instance.GameState())
+        {
+            PlayerStatus();
+            PlayerMovement();
+            SwordControl();
+        }
+    }
+
+    private void LateUpdate()
+    {
+        // Resets player back to last known grounded position
+        if (transform.position.y < -40f)
+        {
+            transform.position = lastPosition;
+        }
+    }
+
+    // Gets animation clip speed and assigns them to floats
+    private void UpdateAnimationClipTimes()
+    {
+        clips = playerAnimator.runtimeAnimatorController.animationClips;
+        foreach (AnimationClip clip in clips)
+        { 
+            switch (clip.name)
+            {
+                case "UI_Main_Dash":
+                    dashClip = clip.length;
+                    break;
+                case "UI_Main_Death":
+                    deathClip = clip.length;
+                    break;
+                case "UI_Main_First_Slash":
+                    firstSlashClip = clip.length;
+                    break;
+                case "UI_Main_Pose_Walk":
+                    poseClip = clip.length;
+                    break;
+                case "UI_Main_Second_Slash":
+                    secondSlashClip = clip.length;
+                    break;
+                case "UI_Main_Shield_Break":
+                    shieldClip = clip.length;
+                    break;
+                case "UI_Main_Spin":
+                    spinClip = clip.length;
+                    break;
+                case "UI_Main_Sword_Pick_Up":
+                    swordPickupClip = clip.length;
+                    break;
+                case "UI_Main_Sword_Throw":
+                    swordThrowClip = clip.length;
+                    break;
+                case "UI_Main_Swordless_Idle":
+                    swordlessIdleClip = clip.length;
+                    break;
+                case "UI_Main_Swordless_Walk":
+                    swordlessWalkClip = clip.length;
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
+    // Retrieves Animator Weight
+    /*
+    private void UpdateLayerWeight()
+    {
+        layers = playerAnimatorController.layers;
+        // remove first element which represents first layer, 
+        //as default weight does not take into account first layer
+        foreach (AnimatorControllerLayer animatorLayer in layers)
+        {
+            // case hold info on different weights
+            Debug.Log(animatorLayer.name);
+            Debug.Log(animatorLayer.defaultWeight);
+        }
+    }
+    */
+
+    // Checks player health
+    private void PlayerStatus()
+    {
+        if (currentPlayerHealth <= 0 || Input.GetKeyDown(KeyCode.G))
+        {
+            Debug.Log("KeyPressed");
+            if (!playerAnimator.GetBool("isDead"))
+            {
+                playerAnimator.SetBool("isDead", true);
+                StartCoroutine(ClipDelay(deathClip));
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            playerAnimator.SetBool("isDead", false);
+            SceneManager.LoadScene("MainScene");
+        }
+        if (Input.GetKeyDown(KeyCode.H)) // playerHit
+        {
+            playerAnimator.SetBool("isHit", true);
+            StartCoroutine(ClipDelay(shieldClip, "isHit"));
+        }
+    }
+
+    // Delays animation transition by clipDuration
+    private IEnumerator ClipDelay(float clipDuration, string animationName = null, bool animState = false)
+    {
+        yield return new WaitForSeconds(clipDuration);
+        StopCoroutine(ClipDelay(clipDuration, animationName));
+        if (animationName != null)
+        {
+            playerAnimator.SetBool(animationName, animState);
+        }
+    }
+
+    // Handles player movement
+    private void PlayerMovement()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask); // true if object hit is within groundmask layer
+        
+        // Keeps the player grounded and updates last know position for respawn
+        if (isGrounded && velocity.y < -2f)
+        {
+            lastPosition = transform.position;
+            velocity.y = -2f; // ensures player is fully grounded since we are using a sphere // keeps velocity from being too large
+        }
+
+        // Call Game Controller to check if controller has been detected and switch controls
+        xAxis = Input.GetAxis("Horizontal"); // left right
+        zAxis = Input.GetAxis("Vertical"); // forward vector
+
+        //Increase or decrease the player movement speed
+        if (zAxis > 0)
+        {
+            //Increase the player speed
+            currentPlayerSpeed += acceleration * Time.deltaTime;
+            // Play footstep sounds
+            AudioController.Instance.PlayRunStep(speedRatio);
+        }
+        else if (zAxis < 0 && currentPlayerSpeed != 0)
+        {
+            AudioController.Instance.PlayRunStep(speedRatio);
+        }
+
+        if (zAxis == 0)
+        {
+            // speed reduced by twice the amount of acceleration
+            currentPlayerSpeed -= acceleration * 2.0f * Time.deltaTime;
+            
+            /* Slide Snippet */
+            //characterController.Move(transform.TransformDirection(Vector3.forward) * Time.deltaTime);
+            // multiply by slide duration on timer so that it stop after some time  
+            // Newton's Second Law of Motion // Possible Inclusion https://www.youtube.com/watch?v=ckm7HhwNDvk
+        }
+
+        //Clamp the speed
+        currentPlayerSpeed = Mathf.Clamp(currentPlayerSpeed, 0f, maxPlayerSpeed);
+
+        // Increase spinner animation and speedometer fill
+        speedRatio = currentPlayerSpeed / maxPlayerSpeed;
+        speedRatio = Mathf.Clamp(speedRatio, 0f, 1f);
+        playerAnimator.SetFloat("spinSpeed", speedRatio);
+        speedometer.fillAmount = speedRatio;
+
+        // Update dash cooldown light
+        float colorTransition = dashTimer / dashCooldown;
+        dashLight.color = Color.Lerp(colorGreen, colorRed, colorTransition);
+        
+
+        Vector3 movement = transform.right * xAxis + transform.forward * zAxis;
+        characterController.Move(movement * currentPlayerSpeed * Time.deltaTime);
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity); // how much velocity we need to jump
+        }
+
+        /* Dash Ability */
+        dashTimer -= Time.deltaTime;
+        dashTimer = Mathf.Clamp(dashTimer, 0f, dashCooldown);
+
+        // GetKeyDown checks for first time key was pressed, GetKey constantly checks and returns interrupt more than once
+        if (Input.GetKeyDown(KeyCode.LeftShift)) 
+        {
+            if (dashTimer <= 0f)
+            {
+                playerAnimator.Play("UI_Main_Dash", 1, 0.3f); 
+                characterController.Move(transform.forward * dashDistance);
+                dashTimer = dashCooldown; // Reset dashTimer when dash is called
+
+                // Play dash effect
+                AudioController.Instance.PlayDash();
+            }
+        }
+        else
+        {
+            
+        }
+        // Freefall formula, applies gravity over time
+        velocity.y += gravity * Time.deltaTime;
+        
+        characterController.Move(velocity * Time.deltaTime); 
+        // End of Freefall
+    }
+
+    private void SwordControl()
+    {
+        
+        if (!isSwinging)
+        {
+            isFirstSlash = true;
+        }
+        else
+        {
+            isFirstSlash = false;
+        }
+        
+        // Left mouse button
+        if (Input.GetKeyDown(KeyCode.Mouse0) && SwordController.Instance.GetHoldingStatus())
+        {
+            if (!isSwinging && isFirstSlash) // first slash
+            {
+                if (playerAnimator.GetCurrentAnimatorStateInfo(1).length > playerAnimator.GetAnimatorTransitionInfo(1).normalizedTime)
+                {
+                    isSwinging = true;
+                    playerAnimator.SetInteger("attackID", 0);
+                    StartCoroutine(SwordAttack(firstSlashClip));
+
+                    // Play first swing effect
+                    if (!playerAnimator.GetCurrentAnimatorStateInfo(1).IsName("UI_Main_First_Slash") && 
+                        !playerAnimator.GetCurrentAnimatorStateInfo(1).IsName("UI_Main_Second_Slash") &&
+                        !AudioController.Instance.isPlaying(AudioController.Instance.PlayFirstSwing()))
+                    {
+                        Debug.Log("First - This should show but it is not being shown!");
+                        AudioController.Instance.PlayFirstSwing();
+                    }
+                }
+            }
+            else if (isSwinging && !isFirstSlash)
+            {
+                if (playerAnimator.GetCurrentAnimatorStateInfo(1).length > playerAnimator.GetAnimatorTransitionInfo(1).normalizedTime) // && !inSwingState()
+                {
+                    playerAnimator.SetInteger("attackID", 1);
+                    StartCoroutine(SwordAttack(secondSlashClip, true));
+                    // Play second swing effect
+                    if (!playerAnimator.GetCurrentAnimatorStateInfo(1).IsName("UI_Main_First_Slash") &&
+                       !playerAnimator.GetCurrentAnimatorStateInfo(1).IsName("UI_Main_Second_Slash") && 
+                       !AudioController.Instance.isPlaying(AudioController.Instance.PlaySecondSwing()))
+                    {
+                        Debug.Log("Second - This should show but it is not being shown!");
+                        AudioController.Instance.PlaySecondSwing();
+                    }
+                }
+                // prevents attack click spam, comvined with exit time in animator properties
+            }
+        }
+        // Right Mouse Button
+        /*
+        if (Input.GetKeyDown(KeyCode.Mouse1) && SwordController.Instance.GetHoldingStatus())
+        {
+            if (!isSwinging) 
+            {
+                Debug.Log("Clicked Right!");
+                isSwinging = true;
+                playerAnimator.SetInteger("attackID", 1);
+                StartCoroutine(SwordAttack(secondSlashClip));
+                //Time.timeScale = 0.25f;
+            }
+        }
+        */
+
+        /* Throw Ability */
+        throwTimer -= Time.deltaTime;
+        throwTimer = Mathf.Clamp(throwTimer, 0f, throwCooldown);
+        // Right mouse button
+        if ((Input.GetKeyDown(KeyCode.Mouse2) || Input.GetKeyDown(KeyCode.F)) && throwTimer <= 0f)
+        {
+            if (playerSword != null)
+            {
+                SwordController.Instance.ThrowSword();
+                throwTimer = throwCooldown;
+
+                // Play sword throw
+                AudioController.Instance.PlaySwordThrow();
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        switch (other.gameObject.name)
+        {
+            case "SwordSpawn":
+                SwordController.Instance.SetHoldingStatus(true);
+                break;
+        }
+    }
+
+    private IEnumerator SwordAttack(float attackTime, bool secondSlash = false)
+    {
+        if (!secondSlash)
+        {
+            yield return new WaitForSeconds(attackTime);
+            isSwinging = false;
+            playerAnimator.SetInteger("attackID", -1);
+            StopCoroutine(SwordAttack(attackTime));
+        }
+        else
+        {
+            yield return new WaitForSeconds(attackTime);
+            isFirstSlash = true;
+            playerAnimator.SetInteger("attackID", -1);
+            StopCoroutine(SwordAttack(attackTime, secondSlash));
+        }
+        
+    }
+
+    public void TakeDamage(float damageAmount)
+    {
+        currentPlayerHealth -= damageAmount;
+        if (currentPlayerHealth == 0)
+        {
+            currentPlayerHealth = 0;
+        }
+    }
+
+    // Returns current player speed
+    public float GetPlayerSpeed()
+    {
+        return currentPlayerSpeed;
+    }
+    
+    public float GetPlayerHealth()
+    {
+        return currentPlayerHealth;
+    }
+    
+    public bool swingState()
+    {
+        return isSwinging;
+    }
+
+}
---
+++ Assets/Scripts/SwordController.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwordController : MonoBehaviour
+{
+    private static SwordController instance;
+    public static SwordController Instance { get { return instance; } }
+
+    // Public Variables
+    public GameObject sword;
+    public float throwForce = 800f;
+    public int swordDamage = 10;
+    public Transform shootPoint;
+    
+
+    // Internal Variables
+    private Vector3 originalPosition;
+    private Quaternion originalRotation;
+    private Vector3 originalScale;
+    private Transform swordParent;
+    private Transform rootParent;
+    private SphereCollider swordCollider;
+    
+    private bool isHolding = true;
+    private bool swordPickupDelay = false;
+    private float pickupDelay = 0.7f;
+
+
+    void Awake()
+    {
+        /* Singleton */
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            DestroyImmediate(gameObject);
+        }
+        /* End of Singleton */
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        originalPosition = transform.localPosition; 
+        originalRotation = transform.localRotation;
+        originalScale = transform.localScale;
+        swordCollider = GetComponentInChildren<SphereCollider>();
+        if (!swordCollider)
+        {
+            Debug.Log("Sword Collider not retrieved!");
+        }
+        swordParent = transform.parent;
+        rootParent = transform.root;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // check if isHolding
+        if (isHolding)
+        {
+            sword.transform.localPosition = originalPosition;
+            sword.transform.localRotation = originalRotation;
+            sword.transform.localScale = originalScale;
+            sword.GetComponent<Rigidbody>().velocity = Vector3.zero;
+            sword.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+            sword.GetComponent<Rigidbody>().useGravity = false;
+            sword.transform.SetParent(swordParent); // right wrist
+            //Time.timeScale = 1f;
+        }
+        else
+        {
+            sword.transform.SetParent(null);
+            //Time.timeScale = 0.4f;
+        }
+    }
+
+    private void LateUpdate()
+    {
+        // Sword teleports to 1,1,1
+        if (transform.position.y < -20f)
+        {
+            transform.position = Vector3.one;
+        }
+    }
+
+    // Hits player, set sword to original holding position
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (!collision.collider.isTrigger)
+        {
+            // when something hit play once, check if not currently playing
+            if (isHolding && PlayerController.Instance.swingState())
+            {
+                // play effect
+                AudioController.Instance.PlaySwordHit();
+                Debug.Log("Sword hit - " + collision.gameObject.name);
+            }
+            else 
+            {
+                // do nothing
+            }
+
+            switch (collision.gameObject.tag)
+            {
+                case "Executioner":
+                    collision.gameObject.GetComponent<BigBoiAI>().TakeDamage(swordDamage);
+                    break;
+                case "Drone":
+                    collision.gameObject.GetComponent<DroneAI>().TakeDamage(swordDamage);
+                    break;
+                default:
+                    break;
+            }
+        }
+        
+        
+        // Damage enemies
+
+        /*
+        // go through all contact points and find out which child collider was involved in collisio
+        Collider myCollider = collision.contacts[0].thisCollider;
+        if (myCollider.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Sword Picked up!");
+            
+        }
+        */
+    }
+
+    // called once when object enters trigger
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player") && !isHolding && swordPickupDelay)
+        {
+            isHolding = true;
+            swordPickupDelay = false;
+            PlayerController.Instance.playerAnimator.SetInteger("attackID", -1); // change to pickup, change to idle?
+        }
+    }
+
+    public void ThrowSword()
+    {
+        if (isHolding) 
+        {
+            StartCoroutine(SwordPickupDelay(pickupDelay));
+            isHolding = false;
+            Transform tempParent = swordParent;
+            swordParent = rootParent; // shoot from Player transform, which is the root parent of this game object
+            sword.transform.position = shootPoint.position;
+            sword.transform.rotation = new Quaternion(Camera.main.transform.rotation.x, swordParent.transform.rotation.y, swordParent.transform.rotation.z, swordParent.transform.rotation.w);
+            //Vector3 localForward = transform.parent.InverseTransformDirection(transform.forward);
+            sword.GetComponent<Rigidbody>().useGravity = true;
+            sword.GetComponent<Rigidbody>().detectCollisions = true;
+            float forceMultiplier = 2f; // 0f
+            
+            sword.GetComponent<Rigidbody>().AddForce(Camera.main.transform.forward * throwForce * forceMultiplier); // swordParent.transform.forward // fires from pivot3
+                                                                                                                  
+            // Call animator to play sword throw clip
+            PlayerController.Instance.playerAnimator.Play("UI_Main_Sword_Throw", 1, 0.5f);
+
+            // reset parent
+            swordParent = tempParent;
+        }
+    }
+
+    private IEnumerator SwordPickupDelay(float pickupDelay)
+    {
+        yield return new WaitForSeconds(pickupDelay);
+        swordPickupDelay = true;
+        StopCoroutine(SwordPickupDelay(pickupDelay));
+    }
+
+    public bool GetHoldingStatus()
+    {
+        return isHolding;
+    }
+
+    public void SetHoldingStatus(bool swordStatus)
+    {
+        isHolding = swordStatus;
+    }
+}
