3b77417ca49c13b2c63c307dfb97ec5e28d5c977
Daniel Zammit <dzamm20@gmail.com>
2020-05-03 16:37:27 +0100

    Added tutorial sprite blinker

---
+++ Assets/Scripts/SpriteBlink.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteBlink : MonoBehaviour
+{
+    public GameObject staticSprite;
+    public GameObject actionSprite;
+    public float blinkTime = 1f;
+
+    void Start()
+    {
+        staticSprite.GetComponent<SpriteRenderer>().enabled = true;
+        actionSprite.GetComponent<SpriteRenderer>().enabled = false;
+        StartCoroutine(StartBlinking()); 
+    }
+
+    IEnumerator StartBlinking()
+    {
+        yield return new WaitForSeconds(blinkTime); 
+        staticSprite.GetComponent<SpriteRenderer>().enabled = !staticSprite.GetComponent<SpriteRenderer>().enabled; //This toggles it
+        actionSprite.GetComponent<SpriteRenderer>().enabled = !actionSprite.GetComponent<SpriteRenderer>().enabled;
+        StartCoroutine(StartBlinking());
+    }
+}
